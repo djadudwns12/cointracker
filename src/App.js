d842b93fecd9032f5ca61244b7a848b446ab34d2
djadudwns12 <djadudwns12@naver.com>
2024-11-06 13:41:07 +0900

    feat : 완료

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Movie from './component/Movie';
 
 // https://api.coinpaprika.com/v1/tickers?limit=10
+// http://localhost:8081/reply/all/1244/1
+// https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=8344c161e851343f33ceb78f3de3fc62&targetDt=
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState('');
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearchMovies = () => {
+    return search === ''
+      ? movies
+      : movies.filter((movie) => movie.movieNm.includes(search));
+  };
 
   useEffect(() => {
     // 비동기 통신의 시점을 정한다.
-    fetch('https://api.coinpaprika.com/v1/tickers?limit=10')
+    fetch(
+      'https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=8344c161e851343f33ceb78f3de3fc62&targetDt=20241105',
+    )
       .then((response) => response.json())
-      .then((data) => setCoins(data));
-    console.log(coins);
+      .then((data) => setMovies(data.boxOfficeResult.dailyBoxOfficeList));
+    console.log(movies);
     setLoading(false);
-    console.log('useEffect!!');
   }, []);
 
   return (
     <div className="App container">
-      <h1>🎮The Coins🎮</h1>
+      <h1>🎮The Movies🎮</h1>
       {loading ? <strong>Loading....</strong> : null}
+
+      <div className="searchBar">
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={onChangeSearch}
+          value={search}
+        />
+        <button>Search</button>
+      </div>
+
+      <ul className="list-group">
+        {getSearchMovies().map((movie) => (
+          <Movie key={movie.movieCd} {...movie} />
+        ))}
+      </ul>
     </div>
   );
 }
