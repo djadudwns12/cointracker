40cd80febc9e3af613bd2c6b4034803cd6115c63
djadudwns12 <djadudwns12@naver.com>
2024-11-06 10:15:36 +0900

    feat : 만드는중

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+// https://api.coinpaprika.com/v1/tickers?limit=10
+
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    // 비동기 통신의 시점을 정한다.
+    fetch('https://api.coinpaprika.com/v1/tickers?limit=10')
+      .then((response) => response.json())
+      .then((data) => setCoins(data));
+    console.log(coins);
+    setLoading(false);
+    console.log('useEffect!!');
+  }, []);
+
+  return (
+    <div className="App container">
+      <h1>🎮The Coins🎮</h1>
+      {loading ? <strong>Loading....</strong> : null}
+    </div>
+  );
 }
 
 export default App;
